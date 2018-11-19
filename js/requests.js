3d6769f8990739dadc84d3d5164c367d45e926ea
JPerfili <justinperfili@gmail.com>
2018-11-19 13:12:41 -0700

    changes to create-item

--- js/requests.js
+++ js/requests.js
@@ -1,127 +1,91 @@
-$("#item-form").submit(function(e) {
+var AssetManager = window.AssetManager || {};
 
-  e.preventDefault()
-  // console.log($(this).serializeArray());
-  // var form = $(this);
-  // console.log(form.serializeArray())
-
-  // $.ajax({
-  //       method: 'POST',
-  //       url: _config.api.invokeURL + '/create-item',
-  //       data: JSON.stringify({
-  //           item: {
-  //               assetID: new_item.assetID,
-  //               text: new_item.text,
-  //               assetLocation: new_item.assetLocation,
-  //               description: new_item.description,
-  //               implementationMonth: new_item.implementationMonth,
-  //               implementationYear: new_item.implementationYear,
-  //               manufacturer: new_item.manufacturer,
-  //               manufacturerPart: new_item.manufacturerPart,
-  //               organizationalTag: new_item.organizationalTag
-  //           }
-  //       }),
-  //       contentType: 'application/json',
-  //       success: completeRequest,
-  //       error: function ajaxError(jqXHR, textStatus, errorThrown) {
-  //           console.error('Error creating new item: ', textStatus, ', Details: ', errorThrown);
-  //           console.error('Response: ', jqXHR.responseText);
-  //           alert('An error occured when creating a new item:\n' + jqXHR.responseText);
-  //       }
-  //   })
-
-    e.preventDefault(); // avoid to execute the actual submit of the form.
-});
-
-var settings = {
-  "async": true,
-  "crossDomain": true,
-  "url": "https://bll0hoveu3.execute-api.us-east-1.amazonaws.com/prod/list-items",
-  "method": "GET",
-}
-
-$.ajax(settings).done(function (response) {
-  console.log(response);
-});
-
-  // console.log(JSON.stringify($('#item-form').serializeArray()))
-  // console.log($("select[name=assignedLocation]").val())
-  // console.log($("input[name=organizationalTag]").val())
-  // console.log($("input[name=manufacturer]").val())
-  // console.log($("input[name=manufacturerPart]").val())
-  // console.log($("input[name=description]").val())
-  // console.log($("select[name=implementationMonth]").val())
-  // console.log($("input[name=implementationYear]").val())
-  // console.log($("textarea[name=maintenanceNotes]").val())
-
-
-
-
-function create_item(new_item) {
-    
-}
-
-function completeRequest(result) {
-    console.log('Response received from API: ', result);
-}
-
-function delete_item(new_item) {
-    $.ajax({
-        method: 'POST',
-        url: _config.api.invokeURL + '/delete-item',
-        data: JSON.stringify({
-            assetID: new_item.assetID
-        }),
-        contentType: 'application/json',
-        success: completeRequest,
-        error: function ajaxError(jqXHR, textStatus, errorThrown) {
-            console.error('Error creating new item: ', textStatus, ', Details: ', errorThrown);
-            console.error('Response: ', jqXHR.responseText);
-            alert('An error occured when creating a new item:\n' + jqXHR.responseText);
-        }
-    })
-}
-
-function get_item(new_item) {
-    $.ajax({
-        method: 'POST',
-        url: _config.api.invokeURL + '/get-item',
-        data: JSON.stringify({
-            assetID: new_item.assetID
-        }),
-        contentType: 'application/json',
-        success: completeRequest,
-        error: function ajaxError(jqXHR, textStatus, errorThrown) {
-            console.error('Error creating new item: ', textStatus, ', Details: ', errorThrown);
-            console.error('Response: ', jqXHR.responseText);
-            alert('An error occured when creating a new item:\n' + jqXHR.responseText);
-        }
-    })
-}
-
-function update_item(new_item) {
-    $.ajax({
-        method: 'POST',
-        url: _config.api.invokeURL + '/update-item',
-        data: JSON.stringify({
-            item: {
-                assetID: new_item.assetID,
-                text: new_item.text,
-                assetLocation: new_item.assetLocation,
-                description: new_item.description,
-                implementationMonth: new_item.implementationMonth,
-                implementationYear: new_item.implementationYear,
-                manufacturer: new_item.manufacturer,
-                manufacturerPart: new_item.manufacturerPart,
-                organizationalTag: new_item.organizationalTag
+(function PostManager($) {
+    function create_item(new_item) {
+        $.ajax({
+            method: 'POST',
+            url: _config.api.invokeURL + '/create-item',
+            data: JSON.stringify({
+                item: {
+                    assetID: new_item.assetID,
+                    assetLocation: new_item.assetLocation,
+                    description: new_item.description,
+                    implementationMonth: new_item.implementationMonth,
+                    implementationYear: new_item.implementationYear,
+                    maintenanceNotes: new_item.maintenanceNotes,
+                    manufacturer: new_item.manufacturer,
+                    manufacturerPart: new_item.manufacturerPart,
+                    organizationalTag: new_item.organizationalTag
+                }
+            }),
+            contentType: 'application/json',
+            success: completeRequest,
+            error: function ajaxError(jqXHR, textStatus, errorThrown) {
+                console.error('Error creating new item: ', textStatus, ', Details: ', errorThrown);
+                console.error('Response: ', jqXHR.responseText);
+                alert('An error occured when creating a new item:\n' + jqXHR.responseText);
             }
-        }),
-        contentType: 'application/json',
-        success: completeRequest,
-        error: function ajaxError(jqXHR, textStatus, errorThrown) {
-            console.error('Error creating new item: ', textStatus, ', Details: ', errorThrown);
-            console.error('Response: ', jqXHR.responseText);
-            alert('An error occured when creating a new item:\n' + jqXHR.responseText);
-        }
-    })
-}
+        })
+    }
+    function completeRequest(result) {
+        console.log('Response received from API: ', result);
+    }
+    function delete_item(new_item) {
+        $.ajax({
+            method: 'POST',
+            url: _config.api.invokeURL + '/delete-item',
+            data: JSON.stringify({
+                assetID: new_item.assetID
+            }),
+            contentType: 'application/json',
+            success: completeRequest,
+            error: function ajaxError(jqXHR, textStatus, errorThrown) {
+                console.error('Error creating new item: ', textStatus, ', Details: ', errorThrown);
+                console.error('Response: ', jqXHR.responseText);
+                alert('An error occured when creating a new item:\n' + jqXHR.responseText);
+            }
+        })
+    }
+    function get_item(new_item) {
+        $.ajax({
+            method: 'POST',
+            url: _config.api.invokeURL + '/get-item',
+            data: JSON.stringify({
+                assetID: new_item.assetID
+            }),
+            contentType: 'application/json',
+            success: completeRequest,
+            error: function ajaxError(jqXHR, textStatus, errorThrown) {
+                console.error('Error creating new item: ', textStatus, ', Details: ', errorThrown);
+                console.error('Response: ', jqXHR.responseText);
+                alert('An error occured when creating a new item:\n' + jqXHR.responseText);
+            }
+        })
+    }
+    function update_item(new_item) {
+        $.ajax({
+            method: 'POST',
+            url: _config.api.invokeURL + '/update-item',
+            data: JSON.stringify({
+                item: {
+                    assetID: new_item.assetID,
+                    assetLocation: new_item.assetLocation,
+                    description: new_item.description,
+                    implementationMonth: new_item.implementationMonth,
+                    implementationYear: new_item.implementationYear,
+                    maintenanceNotes: new_item.maintenanceNotes,
+                    manufacturer: new_item.manufacturer,
+                    manufacturerPart: new_item.manufacturerPart,
+                    organizationalTag: new_item.organizationalTag
+                }
+            }),
+            contentType: 'application/json',
+            success: completeRequest,
+            error: function ajaxError(jqXHR, textStatus, errorThrown) {
+                console.error('Error creating new item: ', textStatus, ', Details: ', errorThrown);
+                console.error('Response: ', jqXHR.responseText);
+                alert('An error occured when creating a new item:\n' + jqXHR.responseText);
+            }
+        })
+    }
+}(jQuery));
