92081c12584c4fe8937e28b2766aa5598698f03a
Mitchell Moody <moodymitch@gmail.com>
2018-11-19 19:32:23 -0700

    fixed creating item

--- js/requests.js
+++ js/requests.js
@@ -15,52 +15,16 @@ $("#item-form").submit(function(e) {
     "url": "https://bll0hoveu3.execute-api.us-east-1.amazonaws.com/prod/create-item",
     "method": "POST",
     "headers": {
-      "Content-Type": "application/json",
+      "Content-Type": "application/json"
     },
     "processData": false,
     "data": JSON.stringify(itemObject)
   }
 
-$.ajax(settings).done(function (response) {
-  console.log(response);
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  });
 });
-
-  // $.ajax({
-  //       method: 'POST',
-  //       url: 'https://bll0hoveu3.execute-api.us-east-1.amazonaws.com/prod//create-item',
-  //       data: JSON.stringify(itemObject),
-  //       contentType: 'application/json',
-  //       success: completeRequest,
-  //       error: function ajaxError(jqXHR, textStatus, errorThrown) {
-  //           console.error('Error creating new item: ', textStatus, ', Details: ', errorThrown);
-  //           console.error('Response: ', jqXHR.responseText);
-  //           alert('An error occured when creating a new item:\n' + jqXHR.responseText);
-  //       }
-  //   })
-});
-
-// var settings = {
-//   "async": true,
-//   "crossDomain": true,
-//   "url": "https://bll0hoveu3.execute-api.us-east-1.amazonaws.com/prod/list-items",
-//   "method": "GET",
-// }
-
-// $.ajax(settings).done(function (response) {
-//   // console.log(response);
-// });
-
-  // console.log(JSON.stringify($('#item-form').serializeArray()))
-  // console.log($("select[name=assignedLocation]").val())
-  // console.log($("input[name=organizationalTag]").val())
-  // console.log($("input[name=manufacturer]").val())
-  // console.log($("input[name=manufacturerPart]").val())
-  // console.log($("input[name=description]").val())
-  // console.log($("select[name=implementationMonth]").val())
-  // console.log($("input[name=implementationYear]").val())
-  // console.log($("textarea[name=maintenanceNotes]").val())
-
-
 
 
 function create_item(new_item) {
